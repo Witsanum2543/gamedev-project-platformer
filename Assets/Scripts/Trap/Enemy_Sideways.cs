3f9df9097eefdf2c276bd59d36b6214acd3b7902
Witsanu <witsanum2543@hotmail.com>
2023-02-24 15:21:56 +0700

    remove bunch of things

--- Assets/Scripts/Trap/Enemy_Sideways.cs
+++ Assets/Scripts/Trap/Enemy_Sideways.cs
@@ -5,34 +5,66 @@ public class Enemy_Sideways : MonoBehaviour
     [SerializeField] private float movementDistance;
     [SerializeField] private float speed;
     [SerializeField] private float damage;
+    [SerializeField] private bool upDown;
 
+    // moving left right
     private bool movingLeft;
     private float leftEdge;
     private float rightEdge;
+
+    // moving up down
+    private bool movingUp;
+    private float upperEdge;
+    private float lowerEdge;
 
     private void Awake()
     {
         leftEdge = transform.position.x - movementDistance;
         rightEdge = transform.position.x + movementDistance;
+
+        upperEdge = transform.position.y - movementDistance;
+        lowerEdge = transform.position.y + movementDistance;
     }
 
     private void Update()
     {
-        if (movingLeft)
+        if (upDown)
         {
-            if (transform.position.x > leftEdge)
+            if (movingUp)
             {
-                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+                if (transform.position.y > upperEdge)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
+                }
+                else movingUp = false;
             }
-            else movingLeft = false;
+            else 
+            {
+                if (transform.position.y < lowerEdge)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+                }
+                else movingUp = true;
+            }
         }
-        else 
+        else
         {
-            if (transform.position.x < rightEdge)
+            if (movingLeft)
             {
-                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+                if (transform.position.x > leftEdge)
+                {
+                    transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+                }
+                else movingLeft = false;
             }
-            else movingLeft = true;
+            else 
+            {
+                if (transform.position.x < rightEdge)
+                {
+                    transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+                }
+                else movingLeft = true;
+            }
         }
     }
 
